b3433d7887310b3f857c6d3039f4f6dd2ba4c528
igorexz <igorashik789@gmail.com>
2020-05-08 21:43:32 +0300

    with shells

--- image_noiser.py
+++ image_noiser.py
@@ -1,29 +1,24 @@
 import numpy as np
 import cv2
 
-for i in range(1, 6):
+for i in range(1, 21):
     img_path = "images/good/" + str(i) + ".jpg" 
 
     img = cv2.imread(img_path)
     mean = 0
     var = 10
-    sigma = var ** 0.5
-    gaussian = np.random.normal(mean, sigma, (224, 224)) #  np.zeros((224, 224), np.float32)
+    sigma = var ** 1.2
+    gaussian = np.random.normal(mean, sigma, (img.shape[0], img.shape[1], 3)) #  np.zeros((224, 224), np.float32)
 
     noisy_image = np.zeros(img.shape, np.float32)
-
-    if len(img.shape) == 2:
-        noisy_image = img + gaussian
-    else:
-        noisy_image[:, :, 0] = img[:, :, 0] + gaussian
-        noisy_image[:, :, 1] = img[:, :, 1] + gaussian
-        noisy_image[:, :, 2] = img[:, :, 2] + gaussian
+    
+    noisy_image = img + gaussian
 
     cv2.normalize(noisy_image, noisy_image, 0, 255, cv2.NORM_MINMAX, dtype=-1)
     noisy_image = noisy_image.astype(np.uint8)
 
-    cv2.imshow("img", img)
-    cv2.imshow("gaussian", gaussian)
-    cv2.imshow("noisy", noisy_image)
+    #cv2.imshow("img", img)
+    #cv2.imshow("gaussian", gaussian)
+    #cv2.imshow("noisy", noisy_image)
 
-    cv2.waitKey(0)
+    cv2.imwrite("images/bad/" + str(i) + ".jpg", noisy_image);
